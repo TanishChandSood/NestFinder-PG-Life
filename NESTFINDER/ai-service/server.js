dabb979ce5dd5615fc11bf6675206c14bb59b6e8
Tanish <sdd2024bt20004@sddgpi.com>
2026-07-21 17:35:02 +0530

    Update server.js

--- NESTFINDER/ai-service/server.js
+++ NESTFINDER/ai-service/server.js
@@ -40,7 +40,7 @@ app.post("/ask-ai", async (req, res) => {
     `;
 
     const response = await ai.models.generateContent({
-      model: 'gemini-2.5-flash',
+      model: 'gemini-3.5-flash',
       contents: systemPrompt,
     });
 
